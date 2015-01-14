c01e685530f9cfd0853f9d74e583498f1299a01a
Dylan Visher <dylan.visher@yale.edu>
2015-01-14 18:43:24 -0500

    changed formatting to exclude empty files

--- dedis-ci-script.go
+++ dedis-ci-script.go
@@ -22,6 +22,10 @@ func main() {
 	for _, fn := range fnames {
 		// go source code must be properly formatted
 		if strings.HasSuffix(fn, ".go") {
+			if _, err := os.Stat(fn); os.IsNotExist(err) {
+				fmt.Printf("no such go file: %s", fn)
+				continue
+			}
 			fmtCmd := exec.Command("gofmt", "-l", fn)
 			fmtCmd.Stderr = os.Stderr
 			out, err := fmtCmd.Output()
